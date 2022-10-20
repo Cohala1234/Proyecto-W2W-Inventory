358d5af8c87099a2246a893ef48cff548398d3a0
darocode <derodriguez074@misena.edu.co>
2022-10-19 22:09:32 -0500

    Arreglo migracion de user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'userName',
         'email',
-        'password',
+        'userPassword',
     ];
 
     /**
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    public function setPasswordAttribute($value){
+        $this->attributes['userPassword'] = bcrypt($value);
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('idUser');
+            $table->string('userName')->nullable();
+            $table->string('userPassword')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('idTypeUser');
             $table->rememberToken();
             $table->timestamps();
         });
