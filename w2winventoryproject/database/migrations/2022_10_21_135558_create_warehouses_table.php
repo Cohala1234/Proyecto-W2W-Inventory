407ce00409ace4e3d01b15f44caffeec57260f83
Cohala1234 <bjescudero@misena.edu.co>
2022-10-21 20:45:08 -0500

    migrate, relations, login-register

---
+++ w2winventoryproject/database/migrations/2022_10_21_135558_create_warehouses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->string('direction')->nulleable();
+            $table->timestamps();
+
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('site_id')->constrained();
+            $table->foreignId('section_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warehouses');
+    }
+};
---
+++ w2winventoryproject/database/migrations/2022_10_21_140008_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameProduct')->nulleable();
+            $table->string('barCode')->nulleable();
+            $table->timestamps();
+
+
+            $table->foreignId('typeProduct_id')->constrained();
+            $table->foreignId('unityMeasurement_id')->constrained();
+            $table->foreignId('section_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
