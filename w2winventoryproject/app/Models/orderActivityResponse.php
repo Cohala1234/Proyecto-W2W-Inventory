40e28630779e438179fb686f8cd3efe7a022ad0f
darocode <derodriguez074@misena.edu.co>
2022-11-14 17:38:48 -0500

    Arreglo migracion

--- w2winventoryproject/app/Models/orderActivityResponse.php
+++ w2winventoryproject/app/Models/orderActivityResponse.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\generalActivity;
+use App\Models\responseActivity;
+use App\Models\workOrder;
+
 class orderActivityResponse extends Model
 {
     use HasFactory;
@@ -15,4 +19,14 @@ class orderActivityResponse extends Model
         'response_activity_id',
         'work_order_id'
     ];
+
+    public function generalActivity(){
+        return $this->belongsTo(generalActivity::class);
+    }
+    public function responseActivity(){
+        return $this->belongsTo(responseActivity::class);
+    }
+    public function workOrder(){
+        return $this->belongsTo(workOrder::class);
+    }
 }
